f20920a79005fbf78730021bd994dbbd3f3ed9a0
Jaimedev12 <89111266+Jaimedev12@users.noreply.github.com>
2025-05-09 00:45:36 -0600

    For 2 clients

--- run_simulation.py
+++ run_simulation.py
@@ -2,10 +2,11 @@ from core.grid import SupermarketGrid
 # from optimization.tabu_search import TabuSearchOptimizer
 from utils.helpers import load_shopping_lists
 import config as cfg
-from core.customer import CustomerSimulator
+from core.customer import CustomerSimulator, SimulationResult
 from optimization.layout_generator import get_grid_object
 from typing import List, Dict, Any, Tuple
 from utils.animate_path import animate_path
+import random
 
 def main():
     # Cargar datos
@@ -18,10 +19,14 @@ def main():
         customer = CustomerSimulator(shopping_list)
         customers.append(customer)
         
-    result = customers[0].simulate(initial_grid)
-    print(result)
 
-    animate_path(initial_grid, result.path, 100)
+    selected_customers = random.sample(customers, 2)
+
+    for i, customer_to_simulate in enumerate(selected_customers):
+        result: SimulationResult = customer_to_simulate.simulate(initial_grid)
+        if result.path: # Check if path is not empty
+            animate_path(initial_grid, result.path, speed=300) # Adjusted speed
+
 
 if __name__ == "__main__":
     main()
